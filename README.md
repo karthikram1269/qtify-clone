1a759946fe95b302548ab9dba51ca9e3f49bb569
Karthik Ram <100182451+karthikram1269@users.noreply.github.com>
2024-10-18 07:23:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+This project was a clone of - `https://qtify-venkykumar.netlify.app/`.
+
+- using react-multi-carousel, react hooks.
+- the commented out code is base version of updated ones.
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './components/Home'
+import ParticularPage from './pages/ParticularPage'
+import NewPage from './pages/NewPage'
+import ErrorPage from './pages/ErrorPage'
+import "./styles/style.css"
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path='/page' element={<ParticularPage/>}/>
+        <Route path='/newpage' element={<NewPage/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+export default App
